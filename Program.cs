60da1e6298e8108f90ac4ff13b6f60730f8e4cf6
maianh29 <nguyenmaianh29204@gmail.com>
2025-03-26 00:56:58 +0700

    upload

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles(); // Tự động tìm index.html
+app.UseStaticFiles();  // Cho phép phục vụ file trong wwwroot
+
+app.Run();
